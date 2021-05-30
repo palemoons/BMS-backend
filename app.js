92ffad68cce1b649a6741e5c1e3a589010ec266b
palemoons <palemoons@foxmail.com>
2021-05-30 15:50:11 +0800

    added login part

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var pool = require('../db/dbConnect');
 const { response } = require('../app');
+const createHttpError = require('http-errors');
+const { token } = require('morgan');
 var router = express.Router();
 
-/* GET users listing. */
+/* get the POST from login page */
 router.post('/', function (req, res, next) {
-  pool.query('SELECT * FROM administrator', (err, rows, fields) => {
+  pool.query('SELECT * FROM administrator', (err, rows) => {
     if (err) throw err;
-    console.log(rows[0].admin_id);
+    rows.forEach((e, i) => {
+      if (e.admin_name === req.body.user) {
+        if (e.password === req.body.pwd) {
+          res.cookie('bms', req.body.user, {
+            maxAge: 1 * 3600000,
+            httpOnly: true
+          });
+          res.status(200).send('ok');
+          return;
+        }
+        else {
+          res.status(401).send('invalid password');
+          return;
+        }
+      }
+      if (i === rows.length - 1) res.status(401).send('invalid username');
+    });
   })
 });
 
